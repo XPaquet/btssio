f7890fec96667f3de6cfb76c5ed067f6659d4e79
XPaquet <xav.paq@free.fr>
2014-06-05 09:22:33 +0200

    Ajout BM E4 source

---
+++ BilletMaster/mobile/connection.php
@@ -0,0 +1,34 @@
+<?php 
+	require $_SERVER['DOCUMENT_ROOT'].'/BilletMaster/class/Connexion.class.php';
+    header('Content-type: application/json');
+	
+	$id = $_POST['id']; 
+	$mdp = $_POST['mdp'];
+	settype($aut, "boolean");
+	$aut = false;
+	$message = null;
+
+	if ($id == "")
+    {
+    	$message = "Vous devez renseigner votre identifiant.";
+    }
+    else if ($mdp == "")
+    {
+    	$message = "Vous devez renseigner votre mot de passe.";
+    }
+    else
+    {
+        $uneConnexion = new Connexion();
+    	$laPersonne = $uneConnexion->GetPersonneByLogin2($id, $mdp);
+
+        if ($laPersonne == null)
+    	{
+    		$message = "Votre login et/ou mot de passe est incorrect !";
+    	}
+    	else
+    	{
+    		$aut = true;
+    		$message = null;
+    	}
+  	}
+  	echo '{"autOk":"'.($aut?'true':'false').'","message":"'.$message.'"}';
